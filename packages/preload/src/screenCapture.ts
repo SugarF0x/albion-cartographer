dbe036cf1ac5ef40494279cdf396653a6c3a3450
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-07-07 20:19:10 +0300

    add dev sample image getter

--- packages/preload/src/screenCapture.ts
+++ packages/preload/src/screenCapture.ts
@@ -1,9 +1,20 @@
 import { GlobalKeyboardListener } from 'node-global-key-listener'
 import type { IGlobalKeyDownMap, IGlobalKeyEvent } from 'node-global-key-listener'
 import { Monitor } from 'node-screenshots'
+import fs from 'node:fs'
 
 const v = new GlobalKeyboardListener()
 const monitors = Monitor.all()
+
+// dev
+const files = fs.readdirSync('sampleImages')
+
+function getRandomImage() {
+  const randomIndex = Math.floor(Math.random() * files.length)
+  const [randomFile] = files.splice(randomIndex, 1)
+  return `data:image/png;base64,${fs.readFileSync(`sampleImages/${randomFile}`, 'base64')}`
+}
+// dev end
 
 export function screenCapture(onCapture: (data: string, mousePos: [number, number]) => void) {
   function listener(e: IGlobalKeyEvent, down: IGlobalKeyDownMap) {
@@ -13,6 +24,12 @@ export function screenCapture(onCapture: (data: string, mousePos: [number, numbe
 
     for (const monitor of monitors) {
       if (!monitor.isPrimary) continue
+
+      // dev
+      onCapture(getRandomImage(), [0, 0])
+      break
+      // dev end
+
       const image = monitor.captureImageSync()
       onCapture(URL.createObjectURL(new Blob([image.toPngSync().buffer], { type: 'image/png' })), e.location!)
       break
