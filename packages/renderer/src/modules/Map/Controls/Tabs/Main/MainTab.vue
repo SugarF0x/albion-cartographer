2a109b1e573a0a1b3955f8c13140a42ddec2f1b5
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-07-17 19:46:34 +0300

    adjust event log size

--- packages/renderer/src/modules/Map/Controls/Tabs/Main/MainTab.vue
+++ packages/renderer/src/modules/Map/Controls/Tabs/Main/MainTab.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
 import { takeRight } from 'lodash'
 import Events from '/@/services/Events'
+import { computed } from 'vue'
+
+const log = computed(() => takeRight(Events.log, 25).join('\n'))
 </script>
 
 <template>
   events (last 25)
-  <pre>{{ JSON.stringify(takeRight(Events.log, 25), null, 2) }}</pre>
+  <pre class="event-log">{{ log }}</pre>
 </template>
 
 <style scoped lang="scss">
-
+.event-log {
+  font-size: 1vw;
+}
 </style>
